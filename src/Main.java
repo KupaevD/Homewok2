fd89dca36f010c4b704c9cfcc01ac45153511b7e
Kupaev Dmitriy <kupaevd@gmail.com>
2023-02-27 18:14:27 +0300

    push

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int about = 18_965;
+        int percent = 20;
+        int bonus = about / percent;
+
+        System.out.println(bonus + " бонусных миль");
+
+
+
+
+    }
+}
